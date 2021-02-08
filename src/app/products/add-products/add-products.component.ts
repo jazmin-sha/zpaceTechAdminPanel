84b67aa51d3d18a58212f18c9d2da2d4fb37180d
jazmin-sha <jazminshanu642k@gmail.com>
2021-02-09 02:50:51 +0530

    add multiple phto

--- src/app/products/add-products/add-products.component.ts
+++ src/app/products/add-products/add-products.component.ts
@@ -24,4 +24,17 @@ export class AddProductsComponent implements OnInit {
    
   }
 
+  urls = [] as  any;
+
+  onSelect(e: any){
+    if (e.target.files) {
+      for (let i = 0; i < File.length; i++) {
+        var reader = new FileReader();
+        reader.readAsDataURL(e.target.files[i]);
+        reader.onload = (events: any) => {
+          this.urls.push(events.target.result);  
+      }
+    }
+  }
+}
 }
